f0d12bef76eeed323b437a39e4f022ad03a39327
Louis Yacksmith <louisyscarleton@gmail.com>
2020-12-16 17:17:42 -0500

    Added all requests to db.js

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -1,1 +1,21 @@
+let db;
+// create a new db request for a "budget" database.
+const request = window.indexedDB.open("budget", 1);
 
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  // create object store called "pending" and set autoIncrement to true
+  db.createObjectStore("pending", { keyPath: "id", autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event);
+};
