f9f87fb2d719a954a4e83855f9e5cd81c9f221cf
Louis Yacksmith <louisyscarleton@gmail.com>
2020-12-16 15:46:30 -0500

    added db.js, fixed service worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,2 +1,27 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "db.js",
+  "index.html",
+  "index.js",
+  "styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  console.log("Your files were pre-cached successfully!");
+  self.skipWaiting();
+});
+
+
